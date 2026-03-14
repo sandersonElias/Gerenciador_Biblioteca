ac4c7327276161121a2451f473f8d26357d09b8e
sandersonElias <sanderson.sae@gmail.com>
2026-03-14 09:22:37 -0300

    Migration tb_reserva

--- Back-End/src/main/java/dev/sanderson/Back_End/repository/ReservaRepository.java
+++ Back-End/src/main/java/dev/sanderson/Back_End/repository/ReservaRepository.java
@@ -4,6 +4,8 @@ import dev.sanderson.Back_End.entity.Livro;
 import dev.sanderson.Back_End.entity.Reserva;
 import dev.sanderson.Back_End.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,16 +14,23 @@ import java.util.Optional;
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
 
-    Optional<Reserva> findFirstByLivroAndStatusOrderByDataReservaAsc(
-            Livro livro,
-            String staus
+    @Query("""
+        SELECT r
+        FROM Reserva r
+        WHERE r.livro = :livro
+        AND r.status = :status
+        ORDER BY r.dataReserva ASC
+    """)
+    Optional<Reserva> buscarPrimeiraReserva(
+            @Param("livro") Livro livro,
+            @Param("status") String status
     );
 
     List<Reserva> findByStatus(String status);
 
-    boolean existsByLivroAndUsuarioAndStatus(
+    boolean existsByLivroAndUserAndStatus(
             Livro livro,
-            User usuario,
+            User user,
             String status
     );
 }
