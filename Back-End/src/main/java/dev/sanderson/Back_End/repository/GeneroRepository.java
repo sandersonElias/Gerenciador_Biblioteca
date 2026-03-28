87396da38278771280a0e674265391fc4c9107ce
sandersonElias <sanderson.sae@gmail.com>
2026-03-28 09:53:34 -0300

    Add query GeneroRepository

--- Back-End/src/main/java/dev/sanderson/Back_End/repository/GeneroRepository.java
+++ Back-End/src/main/java/dev/sanderson/Back_End/repository/GeneroRepository.java
@@ -1,13 +1,20 @@
 package dev.sanderson.Back_End.repository;
 
 import dev.sanderson.Back_End.entity.Genero;
+import dev.sanderson.Back_End.entity.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GeneroRepository extends JpaRepository<Genero, Long> {
 
     Optional<Genero> findByGenero(String genero);
+
+    @Query("SELECT g FROM Genero g WHERE LOWER(g.genero) LIKE LOWER(CONCAT('%', :genero, '%'))")
+    List<Livro> buscarGenero(@Param("genero") String genero);
 }
