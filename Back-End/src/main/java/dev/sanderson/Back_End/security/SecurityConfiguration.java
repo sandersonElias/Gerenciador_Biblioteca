84ad7a8e5d3086f48328032bf8b987243cea0097
sandersonElias <sanderson.sae@gmail.com>
2026-04-19 11:54:53 -0300

    Close /auth/registrar

--- Back-End/src/main/java/dev/sanderson/Back_End/security/SecurityConfiguration.java
+++ Back-End/src/main/java/dev/sanderson/Back_End/security/SecurityConfiguration.java
@@ -44,8 +44,6 @@ public class SecurityConfiguration {
 
                         .requestMatchers(HttpMethod.POST, "/auth").permitAll()
 
-                        .requestMatchers(HttpMethod.POST, "/auth/registrar").permitAll()
-
                         // protegidos
                         .requestMatchers(HttpMethod.POST, "/livro/**")
                         .hasRole("ADMIN")
